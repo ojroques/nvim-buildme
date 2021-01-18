9c9ef0d11cbb81cc59c726bf4a2c88f0167310fe
Olivier Roques <oroques@fastmail.com>
2021-01-18 01:02:50 +0100

    Add main commands

--- lua/buildme.lua
+++ lua/buildme.lua
@@ -4,14 +4,15 @@
 
 -------------------- VARIABLES -----------------------------
 local api, cmd, fn, vim = vim.api, vim.cmd, vim.fn, vim
-local bo = vim.bo
 local fmt = string.format
-local job_id
+local nkeys = api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true)
+local job_buffer, job_id
 
 -------------------- OPTIONS -------------------------------
 local opts = {
   buildfile = '.buildme.sh',
-  shell = fn.getenv('SHELL'),
+  shell = fn.getenv('SHELL') or 'bash',
+  wincmd = '',
 }
 
 -------------------- HELPERS -------------------------------
@@ -21,30 +22,47 @@ local function echo(hlgroup, msg)
   cmd('echohl None')
 end
 
--------------------- INTERFACE -----------------------------
+local function is_running()
+  return job_id and fn.jobwait({job_id}, 0)[1] == -1
+end
+
+-------------------- PUBLIC --------------------------------
+local function edit()
+  local autocmd = 'au BufWritePost <buffer> call jobstart("chmod +x %s")'
+  cmd(fmt('edit %s', opts.buildfile))
+  cmd(fmt(autocmd, opts.buildfile))
+end
+
 local function build()
-  local command = ''
+  local shell = ''
+  if is_running() then
+    echo('ErrorMsg', fmt('A build job is already running (id: %d)', job_id))
+    return
+  end
   if fn.filereadable(opts.buildfile) == 0 then
-    echo('WarningMsg', 'Build file not found')
-    cmd(fmt('edit %s', opts.buildfile))
-    cmd(fmt('au BufWritePost <buffer> call jobstart("chmod +x %s")', opts.buildfile))
+    echo('WarningMsg', fmt('Build file %s not found', opts.buildfile))
+    edit()
     return
   end
-  if job_id and fn.jobwait({job_id}, 0)[1] == -1 then
-    echo('WarningMsg', 'A build job is already running')
-    return
+  if opts.shell ~= '' then
+    shell = fmt('%s ', opts.shell)
   end
-  if opts.shell and opts.shell ~= '' then
-    command = fmt('%s ', opts.shell)
+  if opts.wincmd ~= '' then
+    cmd(opts.wincmd)
   end
-  cmd(fmt('buffer %d', api.nvim_create_buf(true, true)))
-  job_id = fn.termopen(fmt('%s%s', command, opts.buildfile))
+  if not job_buffer or fn.buflisted(job_buffer) == 0 then
+    job_buffer = api.nvim_create_buf(true, true)
+  end
+  cmd(fmt('buffer %d', job_buffer))
+  fn.setbufvar(job_buffer, '&modified', 0)
+  job_id = fn.termopen(fmt('%s%s', shell, opts.buildfile))
+  api.nvim_feedkeys(nkeys, 'n', false)
 end
 
 local function stop()
-  if job_id and fn.jobwait({job_id}, 0)[1] == -1 then
+  if is_running() then
     fn.jobstop(job_id)
-    echo('None', fmt('Stopped job %s', job_id))
+    echo('None', fmt('Stopped job %d', job_id))
     return
   end
   echo('None', 'No job to stop')
@@ -57,6 +75,7 @@ end
 
 ------------------------------------------------------------
 return {
+  edit = edit,
   build = build,
   stop = stop,
   setup = setup,
